6617238b47db28f1387e0af4a5747e151ff2f37b
Nicolas Sroczynski <nicolas.sroczynski@gmail.com>
2023-10-05 11:31:00 -0300

    ServiceAssembly extended daily demo about SAUP Server named pipes | simplified example

---
+++ NamedPipes.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO.Pipes;
+using System.Text;
+
+const string pipeName = "ServerPipe";
+
+using var pipe = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut);
+
+Console.WriteLine("[*] Connecting to server...");
+pipe.Connect();
+Console.WriteLine("[*] Connected to server.");
+
+while (true)
+{
+    Console.Write($"Client > ");
+    var input = Console.ReadLine();
+
+    if (String.IsNullOrEmpty(input)) 
+        continue;
+
+    byte[] bytes = Encoding.UTF8.GetBytes(input);
+    pipe.Write(bytes, 0, bytes.Length);
+
+    var result = ReadMessage(pipe);
+
+    Console.WriteLine(result);
+}
+
+
+string ReadMessage(PipeStream pipe)
+{
+    byte[] buffer = new byte[512];
+    var bufferLength = pipe.Read(buffer, 0, buffer.Length);
+    if (bufferLength == 0)
+    {
+        Console.WriteLine("Connection finished from server side");
+        Environment.Exit(0);
+    }
+
+    return Encoding.UTF8.GetString(buffer, 0, bufferLength);
+}
---
+++ NamedPipes.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Pipes;
+using System.Text;
+
+const string pipe = "ServerPipe";
+
+using var server = new NamedPipeServerStream(pipe, PipeDirection.InOut, 1);
+
+Console.WriteLine("[*] Waiting for client connection.");
+server.WaitForConnection();
+Console.WriteLine("[*] Client connected.");
+
+while (true)
+{
+    var buffer = new byte[512];
+
+    var bufferLength = server.Read(buffer, 0, buffer.Length);
+    if (bufferLength == 0) 
+        break;
+
+    var message = Encoding.UTF8.GetString(buffer, 0, bufferLength);
+
+    Console.WriteLine($"Received: {message}");
+
+    var response = Encoding.UTF8.GetBytes(message.ToUpper());
+    server.Write(response, 0, response.Length);
+}
